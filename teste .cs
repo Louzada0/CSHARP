ba89b76ca00c07c147bca9f4ebfe3d17fa47fea5
Louzada0 <joaopedrinho2509@gmail.com>
2024-11-01 16:07:45 -0400

    Tipos de dados primitivos

---
+++ teste .cs
@@ -0,0 +1,20 @@
+int numero = 10;
+Console.WriteLine("Hello World!");
+
+// Tipos de dados primitivos
+short ano = 2025;
+int contador = 10;
+long populacaoMundial =7800000000
+
+// Numeros de ponto flutuante
+float temperatura = 43.6f;
+double precoProduto =179.90;
+
+// Tipos caracter
+
+char inicial = "W";
+
+// Tipos logicos
+bool estaLogado = true;
+bool ativo = false;
+
