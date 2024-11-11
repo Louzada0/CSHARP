e9e29fb9a883cd69475f2b338eff31265ce62fd0
Louzada0 <joaopedrinho2509@gmail.com>
2024-11-11 14:32:00 -0400

    WHILE

--- estrutura-de-repeticao/Program.cs
+++ estrutura-de-repeticao/Program.cs
@@ -1,12 +1,12 @@
 ﻿//Estrutura de repetição
-random numeroAleatorio = new random();
+Random numeroAleatorio = new Random();
 int numero = numeroAleatorio.Next(1, 100);
 int contador = 0;
-Console.WriteLine($"Número alvo:`{numero}");
+Console.WriteLine($"Número alvo: {numero}");
 //lopp que incrementa o contador até ele atingir um numero alvo
 while (contador != numero)
 {
-    Console.WriteLine($"Contador:{contador}.");
+    Console.WriteLine($"Contador: {contador}.");
     contador++;
 }
 Console.WriteLine($"Contador atingiu o número alvo:{contador}");
