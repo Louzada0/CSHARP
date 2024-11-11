9e6f4c16dd4874af8e7e4ad764bb064f7ff6ae26
Louzada0 <joaopedrinho2509@gmail.com>
2024-11-11 14:50:46 -0400

    Estruturas de repetição do while

--- estrutura-de-repeticao/Program.cs
+++ estrutura-de-repeticao/Program.cs
@@ -1,12 +1,21 @@
 ﻿//Estrutura de repetição
-Random numeroAleatorio = new Random();
-int numero = numeroAleatorio.Next(1, 100);
-int contador = 0;
-Console.WriteLine($"Número alvo: {numero}");
-//lopp que incrementa o contador até ele atingir um numero alvo
-while (contador != numero)
+// Random numeroAleatorio = new Random();
+// int numero = numeroAleatorio.Next(1, 100);
+// int contador = 0;
+// Console.WriteLine($"Número alvo: {numero}");
+// //lopp que incrementa o contador até ele atingir um numero alvo
+// while (contador != numero)
+// {
+//     Console.WriteLine($"Contador: {contador}.");
+//     contador++;
+// }
+// Console.WriteLine($"Contador atingiu o número alvo:{contador}");
+
+//Estruturas de repetição do while
+int numero = 0;
+
+do
 {
-    Console.WriteLine($"Contador: {contador}.");
-    contador++;
-}
-Console.WriteLine($"Contador atingiu o número alvo:{contador}");
+    Console.WriteLine($"Número:{numero}");
+    numero++;
+}while (numero < 10);
