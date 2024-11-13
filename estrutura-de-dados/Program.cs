2cc5ff4f9e6ad2ada2ecc2b5fd1b39a69b7ce849
Jp <joaopedrinho2509@gmail.com>
2024-11-13 17:12:20 -0400

    estrutura-de-dados

---
+++ estrutura-de-dados/Program.cs
@@ -0,0 +1,31 @@
+﻿// int[]meuArray = new int[5];
+// meuArray[0] = 10;
+// meuArray[1] = 20;
+// meuArray[2] = 30;
+// meuArray[3] = 40;
+// meuArray[4] = 50;
+// Console.Writeline("Números no array");
+// for (int i = 0; i < meuArray.length; i++)
+// {
+//     COnsole.Writeline(meuArray [i]);
+// }
+
+// List<string> listaDeNomes = new List<string>{"Jp", "Maria", "Bill",}
+// listaDeNomes Add ("Gatona");
+
+// Console.WriteLine("Nomes na lista:");
+// for (int i = 0; i < listaDeNomes.Count; i++)
+// {
+//     Console.WriteLine(listaDeNomes[i]);
+// }
+// Console.WriteLine("\nApós remover Jp:");
+// string nomeParaRemover = "Jp";
+// bool foiRemovido = listaDeNomes.Remove(nomeParaRemover);
+// if(foiRemovido)
+// {
+//     Console.WriteLine($"{nomeParaRemover}") foi removido da lista");
+// }
+// else
+// {
+//     Console.WriteLine($"{nomeParaRemover}") não foi encontrado na lista");
+// }
