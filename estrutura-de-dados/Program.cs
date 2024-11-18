6973aca3b28747592fad638dd63873b6f7a771b6
lucasfigerno <164235144+lucasfigerno@users.noreply.github.com>
2024-11-18 16:47:09 -0400

    Exercicios

--- estrutura-de-dados/Program.cs
+++ estrutura-de-dados/Program.cs
@@ -29,3 +29,65 @@
 // {
 //     Console.WriteLine($"{nomeParaRemover}") não foi encontrado na lista");
 // }
+
+//Estruturas de dados Hash strings
+// HashSet<string> frutas = new HashSet<string>{"Maçã", "Abacaxi", "Laranja",};
+// frutas.Add("Uva");
+// frutas.Add("Maçã");
+
+// Console.WriteLine("Frutas no conjunto");
+// foreach (string frutas in frutas)
+// {
+//     Console.WriteLine(fruta);
+// }
+
+// Console.WriteLine("\nVerificação de existência:");
+// Console.WriteLine(frutas.Contains("Banana")? "Banana está no conjunto" : "Banana não está no conjunto");
+
+
+//Utilizando Remove
+// Frutas.Remove("Laranja");  //Removeu Laranja da lista
+// Console.WriteLine("\nVerificação de existência:");
+// foreach (string frutas in frutas)
+// {
+//     Console.WriteLine(fruta);
+// }
+
+//Exercicio 1
+
+// int> Somar = 
+
+// int resultado = Somar (1, 2, 3, 4, 5);
+// Console.WriteLine($"Resultado: {resultado}");
+
+
+// Exemplo
+// Func<int, int, int> Somar = (a, b) => a + b;
+
+// int resultado = Somar(1, 2);
+// Console.WriteLine($"Resultado => {resultado}");
+
+// int[] array = { 1, 2, 3, 4, 5 };
+ 
+//         int sum = array.Sum();
+// {
+//     Console.WriteLine("A soma dos números é:");
+// }
+// {
+//     Console.WriteLine(sum);// Não faço a minima ideia do q eu fiz mas funcionou
+// }
+
+//Exercicio 2
+
+// List<string> nomes = new List<string>("Jeane", "Wanderson", "Jeane", "Ryan", "Jeane");
+// int contador = 0
+// foreach (string nome in nomes)
+// {
+//     if nome == "Jeane"
+// }
+// {
+//     contador++;
+// }
+// {
+//     Console.Write("O nome Jeane se repete:");
+// }
