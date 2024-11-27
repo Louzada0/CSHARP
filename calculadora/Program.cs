97b34c789aab4a06d8da1c431c4d4c547e4d6224
Louzada0 <joaopedrinho2509@gmail.com>
2024-11-27 17:13:19 -0400

    exercicio

---
+++ calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Dynamic;
+
+public class Calculadora
+{
+    public bool Soma (get; set;)}
+    public bool Subtrair (get; set;)}
+    public bool Multiplicar (get; set;)}
+    public bool Dividir (get; set;)}
+     
+     public string DadosCalculadora()
+        {
+            return $"Título: {this.Titulo}"+
+                   $"Autor: {this.Autor}";
+        }
+
+    }
